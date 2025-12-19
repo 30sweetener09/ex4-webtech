5e8acdc6cc3ac14d40e59b971900645c02004f44
30sweetener09 <vuongan3467@gmail.com>
2025-12-20 01:27:24 +0700

    adjust .env

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({quiet: true});
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-.connect("mongodb+srv://20224913:20224913@it4409.ajy8kd9.mongodb.net/?appName=it4409")
+.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.error("MongoDB Error:", err));
 
@@ -125,6 +126,7 @@ app.delete("/api/users/:id", async (req, res) => {
     }
 });
 
-app.listen(3001, () => {
-    console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT);
 });
